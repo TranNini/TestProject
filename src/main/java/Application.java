ad878b91658cc902078b28c648818e1650eb4ad2
nini <nini.tran@outlook.com>
2022-08-31 08:10:30 +0200

    Class, casting example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -25,5 +25,20 @@ public class Application {
         stringList.add("test");
         stringList.add("test 2");
         System.out.println(stringList);
+
+        Dog struppi = new Dog(18.2, 86.4, "Struppi", 3, "Dalmatiner");
+
+        struppi.bark();
+        System.out.println(struppi.getBreed());
+        System.out.println(struppi);
+
+        Animal bello = new Dog(15.3, 75.4, "Bello", 5, "Pudel");
+        Dog bello2 = (Dog) bello;
+        bello2.bark();
+
+        //does not work
+        Animal lori = new Cat(5.1, 37.1, "lori", 2, true);
+        Dog lori2 = (Dog) lori;
+        lori2.bark();
     }
 }
